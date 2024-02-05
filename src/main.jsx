b5dcdf4d2afb54b54e473dec3d17307dea4ae255
Megan Wright <meganmwright2018@gmail.com>
2024-02-05 15:02:29 -0600

    rewriting main to deploy with pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; 
+import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/NowLearning-CSA-Tracker"> {/* Set the basename to match your GitHub repository name */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
